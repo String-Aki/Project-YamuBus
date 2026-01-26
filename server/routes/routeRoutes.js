87f63620581f25e7a79f9df0fdc0d28d7e24adcb
String-Aki <deepak.akash055@gmail.com>
2026-01-26 21:17:41 +0530

    Code clean up

--- server/routes/routeRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { createRoute } from '../controllers/routeController.js';
-import { protect } from '../middleware/authMiddleware.js';
-
-const router = express.Router();
-
-router.post('/', protect, createRoute);
-
-export default router;
