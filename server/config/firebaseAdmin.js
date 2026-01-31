9e1244f1d51a80c07f7d4367a25a77478b831b44
String-Aki <deepak.akash055@gmail.com>
2026-02-01 00:40:34 +0530

    Firebase serviceKey is parsed from the env

--- server/config/firebaseAdmin.js
+++ server/config/firebaseAdmin.js
@@ -1,17 +1,32 @@
-import admin from 'firebase-admin';
-import fs from 'fs';
+import admin from "firebase-admin";
+import dotenv from "dotenv";
 
-const keyPath = new URL('../serviceAccountKey.json', import.meta.url);
+dotenv.config();
+let serviceAccount;
 
-try {
-    const serviceAccount = JSON.parse(fs.readFileSync(keyPath,'utf8'));
-    
-    admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount)
-    });
-} catch (error) {
-    console.error('Error initializing firebase Admin SDK:', error);
-    process.exit(1);
+if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+} else {
+  const fs = await import("fs");
+  const path = await import("path");
+  const { fileURLToPath } = await import("url");
+
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  const serviceAccountPath = path.join(__dirname, "../serviceAccountKey.json");
+
+  if (fs.existsSync(serviceAccountPath)) {
+    serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, "utf8"));
+  } else {
+    console.error("Firebase Key not found in Environment or File System!");
+  }
+}
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
 }
 
 export default admin;
